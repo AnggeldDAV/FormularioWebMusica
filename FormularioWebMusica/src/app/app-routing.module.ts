2ae181f1ff9a76e1089685e2f59afdb4437da6bd
AnggeldDAV <anggeld.adav@gmail.com>
2024-04-26 14:32:24 +0200

    routing disco

--- FormularioWebMusica/src/app/app-routing.module.ts
+++ FormularioWebMusica/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormConciertoComponent } from './form-concierto/form-concierto.component';
 import { FormCancionesComponent } from './form-canciones/form-canciones.component'
 import { FormArtistaComponent } from './form-artista/form-artista.component';
+import { FormDiscoComponent } from './form-disco/form-disco.component'
 const routes: Routes = [
   { path: 'formConciertos', component: FormConciertoComponent },
   { path: 'formCanciones', component: FormCancionesComponent },
-  { path: 'formArtista', component: FormArtistaComponent }
+  { path: 'formArtista', component: FormArtistaComponent },
+  { path: 'formDisco', component: FormDiscoComponent }
 ];
 
 @NgModule({
