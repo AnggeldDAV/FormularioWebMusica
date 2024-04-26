5dd15d886c4303b0e7fcd8f5c1abdeca4f0eddd3
AnggeldDAV <anggeld.adav@gmail.com>
2024-04-26 13:53:55 +0200

    validadores finalizado

--- FormularioWebMusica/src/app/form-canciones/form-canciones.component.ts
+++ FormularioWebMusica/src/app/form-canciones/form-canciones.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-form-canciones',
@@ -7,15 +7,31 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrl: './form-canciones.component.css'
 })
 export class FormCancionesComponent {
-  formCanciones = new FormGroup({
-    tituloCanciones: new FormControl(),
-    generoCanciones: new FormControl(),
-    artistaCanciones: new FormControl(),
-    discoCanciones: new FormControl(),
-    anyoCanciones: new FormControl(),
-  })
+  formCanciones: FormGroup;
+  mensaje: string = "";
+  constructor(private fb: FormBuilder) {
+
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth();
+    let aaaa = today.getFullYear();
+    let fecha = `${aaaa}-${mm}-${dd}`;
+    
+    this.formCanciones = this.fb.group({
+      tituloCanciones: ['', [Validators.required, Validators.pattern('^[A-Za-z ]+$'), Validators.minLength(3), Validators.maxLength(10)]],
+      generoCanciones: ['', [Validators.required, Validators.pattern('^[A-Za-z ]+$'), Validators.minLength(3), Validators.maxLength(10)]],
+      artistaCanciones: ['', [Validators.required, Validators.pattern('^[A-Za-z ]+$'), Validators.minLength(3), Validators.maxLength(10)]],
+      discoCanciones: ['', [Validators.required, Validators.pattern('^[A-Za-z ]+$'), Validators.minLength(3), Validators.maxLength(10)]],
+      anyoCanciones: [fecha]
+    });
+  }
 
   onSubmit() {
-    console.log(this.formCanciones.value);
+    if (this.formCanciones.valid) {
+      this.mensaje = "Formulario valido"
+      console.log(this.formCanciones.value);
+    } else {
+      this.mensaje = "Formulario invalido"
+    }
   }
 }
