a91f5dfb0677dcf76e5d8ec48ceb085c5fd277b8
Majose <mariajose.paesa@gmail.com>
2024-04-26 14:07:48 +0200

    Creacion formulario Artistas

--- FormularioWebMusica/src/app/app-routing.module.ts
+++ FormularioWebMusica/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormArtistaComponent } from './form-artista/form-artista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "artista", component: FormArtistaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FormularioWebMusica/src/app/app.module.ts
+++ FormularioWebMusica/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormArtistaComponent } from './form-artista/form-artista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormArtistaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
