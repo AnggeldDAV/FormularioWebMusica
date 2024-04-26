28a4441dba2f2742ff27c1e4b1ea5882e9a60234
AnggeldDAV <anggeld.adav@gmail.com>
2024-04-26 14:24:51 +0200

    routing canciones

--- FormularioWebMusica/src/app/app-routing.module.ts
+++ FormularioWebMusica/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormConciertoComponent } from './form-concierto/form-concierto.component';
+import { FormCancionesComponent } from './form-canciones/form-canciones.component'
 
 const routes: Routes = [
-  { path: 'formConciertos', component: FormConciertoComponent }
+  { path: 'formConciertos', component: FormConciertoComponent },
+  { path: 'formCanciones', component: FormCancionesComponent }
 ];
 
 @NgModule({
